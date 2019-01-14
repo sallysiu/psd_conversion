8bf1003942730f8ab9a43ceb8d724a68e809bae2
Sally Siu <sally.siu4149@gmail.com>
2019-01-14 13:53:30 -0500

    added mobile menu

--- scripts/script.js
+++ scripts/script.js
@@ -5,4 +5,14 @@ $(function () {
 		speed: 520,
 		autoFocus: true
 	});
+
+
+	// when a user clicks on an anchor tag inside of the ul with the class of nav do this:
+	$('ul.nav a').on('click', function () {
+		// change the input type checkbox's checked attribute to be false(as if a user unchecked it)
+		$('input[type="checkbox"]').attr('checked', false);
+	});
+
+
+
 });
